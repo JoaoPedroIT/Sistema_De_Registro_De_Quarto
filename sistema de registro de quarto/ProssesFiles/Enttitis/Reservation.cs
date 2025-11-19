d0c71c4b717c1981c6d443e47cbae2320bbdfdad
JoaoPedroIT <joaopitavarasss0025@gmail.com>
2025-11-18 21:58:18 -0300

    Add files via upload

---
+++ sistema de registro de quarto/ProssesFiles/Enttitis/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProssesFiles.Enttitis
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public override string ToString()
+        {
+            return "Room " 
+                + RoomNumber 
+                + ", check-in: " 
+                + CheckIn.ToString("dd/MM/yyyy") 
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy") 
+                + ", " + Duration() 
+                + " nights";
+        }
+
+        internal void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
